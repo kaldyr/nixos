2338d39c97650cab03955b82bdab4ee4ffd27536
kaldyr <kaldyr@gmail.com>
2024-04-13 09:59:29 -0700

    Librewolf fix user agent, update flake

--- home/programs/librewolf.nix
+++ home/programs/librewolf.nix
@@ -68,6 +68,7 @@
             "browser.startup.page" = 3;
             "extensions.unifiedExtensions.enabled" = false;
             "general.useragent.override" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.";
+            "general.platform.override" = "Win32";
             "identity.fxaccounts.enabled" = true;
             "media.ffmpeg.vaapi.enabled" = true;
             "network.cookie.lifetimePolicy" = 0;
