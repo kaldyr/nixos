f8faffe00af03de2f279652b1c98a242f4ee83b7
kaldyr <kaldyr@gmail.com>
2024-08-16 16:32:30 -0700

    formatting

--- programs/kodi.nix
+++ programs/kodi.nix
@@ -4,22 +4,28 @@
     networking.firewall.allowedUDPPorts = [ 8080 9090 ];
 
     services = {
+
         xserver = {
+
             enable = true;
 
             desktopManager.kodi = {
+
                 enable = true;
+
                 package = (pkgs.kodi-wayland.withPackages (kodiPackages: with kodiPackages; [
                     inputstream-adaptive
                     sendtokodi
                     youtube
                 ]));
+
             };
 
             displayManager.autoLogin.enable = true;
             displayManager.autoLogin.user = "kodi";
 
         };
+
     };
 
     users.extraUsers.kodi.isNormalUser = true;
