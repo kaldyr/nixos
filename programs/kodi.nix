42ae4ee138d5316ddba56dc545a12008185b51df
kaldyr <kaldyr@gmail.com>
2024-08-17 00:36:23 -0700

    Kodi tweaks, add persist

--- programs/kodi.nix
+++ programs/kodi.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ lib, pkgs, sysConfig, ... }: {
+
+    environment.persistence = lib.mkIf sysConfig.impermanence {
+        "/state".users.kodi.directories = [
+            { directory = ".cache"; mode = "0700"; }
+            ".compose-cache"
+            ".kodi"
+        ];
+    };
 
     networking.firewall.allowedTCPPorts = [ 8080 9090 ];
     networking.firewall.allowedUDPPorts = [ 8080 9090 ];
@@ -19,7 +27,7 @@
                 package = (pkgs.kodi-wayland.withPackages (kodiPackages: with kodiPackages; [
                     inputstream-adaptive
                     sendtokodi
-                    youtube
+                    # youtube
                 ]));
 
             };
