0081179771b62ac09a12ba4059f40942291625a6
kaldyr <kaldyr@gmail.com>
2024-09-14 23:59:16 -0700

    Nextcloud 29.0.7 fix

--- overlays.nix
+++ overlays.nix
@@ -3,7 +3,7 @@
     # additions = final: _prev: import ./pkgs { pkgs = final; };
 
     modifications = final: prev: {
-        final.pkgs.nextcloud29= inputs.nixpkgs-fresh.pkgs.nextcloud29;
+        nextcloud29 = inputs.nixpkgs-fresh.legacyPackages.${prev.system}.nextcloud29;
     };
 
 }
