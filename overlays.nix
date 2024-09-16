c1987e681e1e0b82c7109bce4b32b56dc003249c
kaldyr <kaldyr@gmail.com>
2024-09-16 13:21:35 -0700

    Update nextcloud to 30

--- overlays.nix
+++ overlays.nix
@@ -3,7 +3,7 @@
     # additions = final: _prev: import ./pkgs { pkgs = final; };
 
     modifications = final: prev: {
-        nextcloud29 = inputs.nixpkgs-fresh.legacyPackages.${prev.system}.nextcloud29;
+        # PACKAGE = inputs.nixpkgs-fresh.legacyPackages.${prev.system}.PACKAGE;
     };
 
 }
