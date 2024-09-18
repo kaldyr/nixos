20dcf1420732fa4da6f017c269c3d43a2915dd2e
kaldyr <kaldyr@gmail.com>
2024-09-18 13:18:36 -0700

    Update foot colorscheme

--- programs/foot.nix
+++ programs/foot.nix
@@ -8,24 +8,35 @@
 
         xdg.configFile."foot/foot.ini".text = /* ini */ ''
             [colors]
-            background=303446
-            bright0=626880
-            bright1=e78284
-            bright2=a6d189
-            bright3=e5c890
-            bright4=8caaee
-            bright5=f4b8e4
-            bright6=81c8be
-            bright7=a5adce
-            foreground=c6d0f5
-            regular0=51576d
-            regular1=e78284
-            regular2=a6d189
-            regular3=e5c890
-            regular4=8caaee
-            regular5=f4b8e4
-            regular6=81c8be
-            regular7=b5bfe2
+            foreground=c6d0f5 #c6d0f5
+            background=303446 #303446
+
+            regular0=51576d #51576d
+            regular1=e78284 #e78284
+            regular2=a6d189 #a6d189
+            regular3=e5c890 #e5c890
+            regular4=8caaee #8caaee
+            regular5=f4b8e4 #f4b8e4
+            regular6=81c8be #81c8be
+            regular7=b5bfe2 #b5bfe2
+
+            bright0=626880 #626880
+            bright1=e78284 #e78284
+            bright2=a6d189 #a6d189
+            bright3=e5c890 #e5c890
+            bright4=8caaee #8caaee
+            bright5=f4b8e4 #f4b8e4
+            bright6=81c8be #81c8be
+            bright7=a5adce #a5adce
+
+            selection-foreground=c6d0f5 #c6d0f5
+            selection-background=4f5369 #4f5369
+
+            search-box-no-match=232634 e78284 #232634 #e78284
+            search-box-match=c6d0f5 414559 #c6d0f5 #414559
+
+            jump-labels=232634 ef9f76 #232634 #ef9f76
+            urls=8caaee #8caaee
 
             [cursor]
             beam-thickness=1
