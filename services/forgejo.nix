3a3ac29a7a3b5ca0ffd95d87099985761e1b0517
kaldyr <kaldyr@gmail.com>
2024-10-11 01:01:54 -0700

    Forgejo setup

--- services/forgejo.nix
+++ services/forgejo.nix
@@ -7,7 +7,46 @@
     services = {
 
         forgejo = {
+
             enable = true;
+
+            user = "forgejo";
+            group = "forgejo";
+            stateDir = "/var/lib/forgejo";
+
+            database = {
+                name = "forgejo";
+                type = "postgres";
+                user = "forgejo";
+            };
+
+            dump.enable = false;
+
+            lfs.enable = true;
+
+            settings = {
+
+                DEFAULT.APP_NAME = "Personal Forge";
+
+                log.LEVEL = "Warn";
+
+                repository.ENABLE_PUSH_CREATE_USER = true;
+                repository.DEFAULT_BRANCH = "main";
+
+                server = {
+                    DOMAIN = "git.magrathea.brill-godzilla.ts.net";
+                    ROOT_URL = "https://git.magrathea.brill-godzilla.ts.net";
+                    HTTP_ADDR = "127.0.0.1";
+                    HTTP_PORT = 9090;
+                };
+
+                # Comment out for initial install
+                service.DISABLE_REGISTRATION = true;
+
+                session.COOKIE_SECURE = true;
+
+            };
+
         };
 
         postgresql.ensureDatabases = [ "forgejo" ];
