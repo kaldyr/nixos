3e8d693bc15fad11fa7428ff2c79782a1de522fa
kaldyr <kaldyr@gmail.com>
2024-10-26 13:41:18 -0700

    Razer tweaks

--- services/openrazer.nix
+++ services/openrazer.nix
@@ -5,9 +5,33 @@
         enable = true;
 
         devicesOffOnScreensaver = true;
-        users = [ "${sysConfig.user}" ];
 
     };
+
+    home-manager.users.${sysConfig.user}.xdg.configFile."openrazer/persistence.conf".text = /* ini */ ''
+        [632423H20104252]
+        dpi_x = 800
+        dpi_y = 800
+        poll_rate = 1000
+        backlight_active = True
+        backlight_brightness = 100
+        backlight_effect = wave
+        backlight_colors = 255 255 255 255 255 255 255 255 255
+        backlight_speed = 1
+        backlight_wave_dir = 2
+        logo_active = True
+        logo_brightness = 100
+        logo_effect = wave
+        logo_colors = 255 255 255 255 255 255 255 255 255
+        logo_speed = 1
+        logo_wave_dir = 2
+        scroll_active = True
+        scroll_brightness = 100
+        scroll_effect = wave
+        scroll_colors = 255 255 255 255 255 255 255 255 255
+        scroll_speed = 1
+        scroll_wave_dir = 2
+    '';
 
     users.users.${sysConfig.user}.extraGroups = [ "openrazer" ];
 
