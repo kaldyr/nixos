5ae59b8e1c6e9bce75cd1887d3f96e3b1b85b1a4
kaldyr <kaldyr@gmail.com>
2024-10-28 03:04:06 -0700

    Zellij keybinds

--- programs/zellij.nix
+++ programs/zellij.nix
@@ -13,95 +13,46 @@
 
             keybinds {
                 locked {
-                    bind "Alt g" {
-                        SwitchToMode "normal"
-                    }
+                    bind "Alt g" { SwitchToMode "normal"; }
                 }
                 move {
-                    bind "Alt m" {
-                        SwitchToMode "normal"
-                    }
+                    bind "Alt m" { SwitchToMode "normal"; }
                 }
                 normal {
-                    bind "Alt 0" {
-                        GoToTab 10 
-                    }
-                    bind "Alt 1" {
-                        GoToTab 1 
-                    }
-                    bind "Alt 2" {
-                        GoToTab 2 
-                    }
-                    bind "Alt 3" {
-                        GoToTab 3 
-                    }
-                    bind "Alt 4" {
-                        GoToTab 4 
-                    }
-                    bind "Alt 5" {
-                        GoToTab 5 
-                    }
-                    bind "Alt 6" {
-                        GoToTab 6 
-                    }
-                    bind "Alt 7" {
-                        GoToTab 7 
-                    }
-                    bind "Alt 8" {
-                        GoToTab 8 
-                    }
-                    bind "Alt 9" {
-                        GoToTab 9 
-                    }
-                    bind "Alt g" {
-                        SwitchToMode "locked" 
-                    }
-                    bind "Alt m" {
-                        SwitchToMode "move" 
-                    }
-                    bind "Alt n" {
-                        SwitchToMode "resize" 
-                    }
-                    bind "Alt o" {
-                        SwitchToMode "session" 
-                    }
-                    bind "Alt p" {
-                        SwitchToMode "pane" 
-                    }
-                    bind "Alt q" {
-                        SwitchToMode "scroll" 
-                    }
-                    bind "Alt s" {
-                        SwitchToMode "scroll" 
-                    }
-                    bind "Alt t" {
-                        SwitchToMode "tab" 
-                    }
+                    bind "Alt 0" { GoToTab 10; }
+                    bind "Alt 1" { GoToTab 1; }
+                    bind "Alt 2" { GoToTab 2; }
+                    bind "Alt 3" { GoToTab 3; }
+                    bind "Alt 4" { GoToTab 4; }
+                    bind "Alt 5" { GoToTab 5; }
+                    bind "Alt 6" { GoToTab 6; }
+                    bind "Alt 7" { GoToTab 7; }
+                    bind "Alt 8" { GoToTab 8; }
+                    bind "Alt 9" { GoToTab 9; }
+                    bind "Alt g" { SwitchToMode "locked"; }
+                    bind "Alt m" { SwitchToMode "move"; }
+                    bind "Alt n" { SwitchToMode "resize"; }
+                    bind "Alt o" { SwitchToMode "session"; }
+                    bind "Alt p" { SwitchToMode "pane"; }
+                    bind "Alt q" { SwitchToMode "scroll"; }
+                    bind "Alt s" { SwitchToMode "scroll"; }
+                    bind "Alt t" { SwitchToMode "tab"; }
                 }
                 pane {
-                    bind "Alt p" {
-                        SwitchToMode "normal" 
-                    }
+                    bind "Alt p" { SwitchToMode "normal"; }
                 }
                 resize {
-                    bind "Alt r" {
-                        SwitchToMode "normal" 
-                    }
+                    bind "Alt r" { SwitchToMode "normal"; }
                 }
                 scroll {
-                    bind "Alt s" {
-                        SwitchToMode "normal" 
-                    }
+                    bind "Alt s" { SwitchToMode "normal"; }
                 }
                 session {
-                    bind "Alt o" {
-                        SwitchToMode "normal" 
-                    }
+                    bind "Alt o" { SwitchToMode "normal"; }
+                    bind "q" { Quit; }
                 }
                 tab {
-                    bind "Alt t" {
-                        SwitchToMode "normal" 
-                    }
+                    bind "Alt t" { SwitchToMode "normal"; }
                 }
                 unbind "Ctrl b" "Ctrl c" "Ctrl f" "Ctrl g" "Ctrl h" "Ctrl n" "Ctrl o" "Ctrl p" "Ctrl q" "Ctrl s" "Ctrl t"
             }
