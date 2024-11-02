5a3cb41361e6e6ac498a7d60e3de2607dda6c957
kaldyr <kaldyr@gmail.com>
2024-11-02 15:04:47 -0700

    Wezterm keybinds

--- programs/wezterm.nix
+++ programs/wezterm.nix
@@ -75,37 +75,124 @@
 
             config.disable_default_key_bindings = true
             config.keys = {
-                { key = '-', mods = 'ALT', action = wezterm.action.DecreaseFontSize };
-                { key = '=', mods = 'ALT', action = wezterm.action.IncreaseFontSize };
-                { key = 'y', mods = 'ALT', action = wezterm.action.QuickSelect };
-                { key = 'u', mods = 'ALT', action = wezterm.action.CharSelect };
-                { key = 'e', mods = 'ALT', action = wezterm.action.EmitEvent 'trigger-nvim-with-scrollback' };
-                { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' };
-                { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' };
-                { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' };
-                { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' };
-                { key = 'LeftArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Left', 1 } };
-                { key = 'DownArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Down', 1 } };
-                { key = 'UpArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Up', 1 } };
-                { key = 'RightArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Right', 1 } };
-                { key = 'p', mods = 'ALT', action = wezterm.action.SplitHorizontal( { domain = 'CurrentPaneDomain' } ) };
-                { key = 'v', mods = 'ALT', action = wezterm.action.SplitVertical( { domain = 'CurrentPaneDomain' } ) };
-                { key = 'm', mods = 'ALT', action = wezterm.action.TogglePaneZoomState };
-                { key = 't', mods = 'ALT', action = wezterm.action.SpawnTab 'DefaultDomain' };
-                { key = '.', mods = 'ALT', action = wezterm.action.ActivateTabRelative( 1 ) };
-                { key = ',', mods = 'ALT', action = wezterm.action.ActivateTabRelative( -1 ) };
-                { key = '>', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative( 1 ) };
-                { key = '<', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative( -1 ) };
-                { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab( 0 ) };
-                { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab( 1 ) };
-                { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab( 2 ) };
-                { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab( 3 ) };
-                { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab( 4 ) };
-                { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab( 5 ) };
-                { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab( 6 ) };
-                { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab( 7 ) };
-                { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab( 8 ) };
-                { key = '0', mods = 'ALT', action = wezterm.action.ActivateTab( 9 ) };
+                { key = '-', mods = 'ALT', action = wezterm.action.DecreaseFontSize },
+                { key = '=', mods = 'ALT', action = wezterm.action.IncreaseFontSize },
+                { key = 'y', mods = 'ALT', action = wezterm.action.QuickSelect },
+                { key = 'u', mods = 'ALT', action = wezterm.action.CharSelect },
+                { key = 'e', mods = 'ALT', action = wezterm.action.EmitEvent 'trigger-nvim-with-scrollback' },
+                { key = 'p',
+                    mods = 'ALT',
+                    action = wezterm.action.ActivateKeyTable {
+                        name = 'create_pane',
+                        timeout_milliseconds = 5000,
+                    },
+                },
+                { key = 's',
+                    mods = 'ALT',
+                    action = wezterm.action.PaneSelect {
+                        mode = 'SwapWithActiveKeepFocus',
+                    },
+                },
+                { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+                { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+                { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
+                { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+                { key = 'LeftArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
+                { key = 'DownArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
+                { key = 'UpArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
+                { key = 'RightArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
+                { key = 'f', mods = 'ALT', action = wezterm.action.TogglePaneZoomState },
+                { key = 'x', mods = 'ALT', action = wezterm.action.CloseCurrentPane { confirm = true } },
+                { key = 't', mods = 'ALT', action = wezterm.action.SpawnTab 'DefaultDomain' },
+                { key = '.', mods = 'ALT', action = wezterm.action.ActivateTabRelative( 1 ) },
+                { key = ',', mods = 'ALT', action = wezterm.action.ActivateTabRelative( -1 ) },
+                { key = '>', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative( 1 ) },
+                { key = '<', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative( -1 ) },
+                { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab( 0 ) },
+                { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab( 1 ) },
+                { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab( 2 ) },
+                { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab( 3 ) },
+                { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab( 4 ) },
+                { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab( 5 ) },
+                { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab( 6 ) },
+                { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab( 7 ) },
+                { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab( 8 ) },
+                { key = '0', mods = 'ALT', action = wezterm.action.ActivateTab( 9 ) },
+            }
+
+            config.key_tables = {
+                create_pane = {
+                    { key = 'Escape', action = "PopKeyTable" },
+                    { key = 'Enter', action = "PopKeyTable" },
+                    { key = 'h',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Left',
+                                size = { Percent = 50 },
+                            }
+                        )
+                    },
+                    { key = 'j',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Down',
+                                size = { Percent = 50 },
+                            }
+                        )
+                    },
+                    { key = 'k',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Up',
+                                size = { Percent = 50 },
+                            }
+                        )
+                    },
+                    { key = 'l',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Right',
+                                size = { Percent = 50 },
+                            }
+                        )
+                    },
+                    { key = 'LeftArrow',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Left',
+                                size = { Percent = 50 },
+                                top_level = true,
+                            }
+                        )
+                    },
+                    { key = 'DownArrow',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Down',
+                                size = { Percent = 50 },
+                                top_level = true,
+                            }
+                        )
+                    },
+                    { key = 'UpArrow',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Up',
+                                size = { Percent = 50 },
+                                top_level = true,
+                            }
+                        )
+                    },
+                    { key = 'RightArrow',
+                        action = wezterm.action.SplitPane(
+                            {
+                                direction = 'Right',
+                                size = { Percent = 50 },
+                                top_level = true,
+                            }
+                        )
+                    },
+                }
             }
 
             config.scrollback_lines = 10000
