cf20e3c57f4613571a389534c54767001b886d22
kaldyr <kaldyr@gmail.com>
2024-11-30 14:22:23 -0800

    Why is yaml so stupid?

--- programs/lazygit.nix
+++ programs/lazygit.nix
@@ -7,31 +7,30 @@
 		programs.lazygit.enable = true;
 
 		xdg.configFile."lazygit/config.yml".text = /* yaml */ ''
-			gui:
-				theme:
-					activeBorderColor:
-						- '#85c1dc'
-						- bold
-					cherryPickedCommitBgColor:
-						- '#51576d'
-					cherryPickedCommitFgColor:
-						- '#85c1dc'
-					defaultFgColor:
-						- '#c6d0f5'
-					inactiveBorderColor:
-						- '#a5adce'
-					optionsTextColor:
-						- '#8caaee'
-					searchingActiveBorderColor:
-						- '#e5c890'
-					selectedLineBgColor:
-						- '#414559'
-					selectedRangeBgColor:
-						- '#414559'
-					unstagedChangesColor:
-						- '#e78284'
-		'';
-
+gui:
+  theme:
+    activeBorderColor:
+      - '#85c1dc'
+      - bold
+    cherryPickedCommitBgColor:
+      - '#51576d'
+    cherryPickedCommitFgColor:
+      - '#85c1dc'
+    defaultFgColor:
+      - '#c6d0f5'
+    inactiveBorderColor:
+      - '#a5adce'
+    optionsTextColor:
+      - '#8caaee'
+    searchingActiveBorderColor:
+      - '#e5c890'
+    selectedLineBgColor:
+      - '#414559'
+    selectedRangeBgColor:
+      - '#414559'
+    unstagedChangesColor:
+      - '#e78284'
+'';
 	};
 
 }
