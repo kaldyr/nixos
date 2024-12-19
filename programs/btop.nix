daa7132fadb198724a8b977bbbdfaf7f87a5eef5
kaldyr <kaldyr@gmail.com>
2024-12-18 22:51:51 -0800

    Fix Typo

--- programs/btop.nix
+++ programs/btop.nix
@@ -2,7 +2,7 @@
 
     home-manager.users.${sysConfig.user} = { config, ... }: {
         programs.btop.enable = true;
-        xdg.configFile."btop".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/bat";
+        xdg.configFile."btop".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/btop";
         xdg.desktopEntries.btop = { name = "btop++"; noDisplay = true; };
     };
 
