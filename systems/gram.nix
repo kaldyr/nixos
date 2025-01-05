97846af9fe0a1566d48774b7b5bc07ab076f62bb
kaldyr <kaldyr@gmail.com>
2025-01-04 20:34:27 -0800

    Disable ghostty and tmux for gram

--- systems/gram.nix
+++ systems/gram.nix
@@ -6,14 +6,14 @@
         ../disko/gram.nix
         ./desktop.nix
         ../services/mpd.nix
-        ../programs/ghostty.nix
+        # ../programs/ghostty.nix
         ../programs/hyprland.nix
         ../programs/lutris.nix
         ../programs/nextcloud-desktop.nix
         ../programs/openscad.nix
         ../programs/plymouth.nix
         ../programs/steam.nix
-        ../programs/tmux.nix
+        # ../programs/tmux.nix
         ../programs/wezterm.nix
     ];
 
