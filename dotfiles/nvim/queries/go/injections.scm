e64957124be4c76bb08f1315b53feb435bfde836
kaldyr <kaldyr@gmail.com>
2025-01-07 11:02:14 -0800

    Working sql injections into go

---
+++ dotfiles/nvim/queries/go/injections.scm
@@ -0,0 +1,10 @@
+(call_expression
+  (selector_expression
+	 operand: (identifier) @operand (#any-of? @operand "db" "DB")
+	 field: (field_identifier) @field (#any-of? @field "Exec" "NamedExec" "QueryRow" "Select" "Get" "Query"))
+
+  (argument_list
+	(raw_string_literal (raw_string_literal_content) @injection.content)
+	(#set! injection.language "sql")
+  )
+)
