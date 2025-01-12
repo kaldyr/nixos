5fec14f019882476a523966dd561f7438e256327
kaldyr <kaldyr@gmail.com>
2025-01-11 16:29:47 -0800

    Tweaks to sql injection

--- dotfiles/nvim/queries/go/injections.scm
+++ dotfiles/nvim/queries/go/injections.scm
@@ -1,10 +1,12 @@
 (call_expression
-  (selector_expression
-	 operand: (identifier) @operand (#any-of? @operand "db" "DB")
-	 field: (field_identifier) @field (#any-of? @field "Exec" "NamedExec" "QueryRow" "Select" "Get" "Query"))
-
-  (argument_list
-	(raw_string_literal (raw_string_literal_content) @injection.content)
-	(#set! injection.language "sql")
-  )
+	(selector_expression
+	operand: (identifier) @operand (#any-of? @operand "db" "DB")
+	field: (field_identifier) @field (#any-of? @field "Exec" "NamedExec" "QueryRow" "Select" "Get" "Query"))
+	(argument_list
+		([
+			(raw_string_literal (raw_string_literal_content) @injection.content)
+			(interpreted_string_literal (interpreted_string_literal_content) @injection.content)
+		])
+		(#set! injection.language "sql")
+	)
 )
