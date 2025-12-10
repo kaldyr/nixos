948f739a7d255ea401dc5bfd09366745651deef2
kaldyr <kaldyr@gmail.com>
2025-12-09 17:01:59 -0800

    Prepare for hardware swap

--- systems/mjolnir.nix
+++ systems/mjolnir.nix
@@ -3,7 +3,7 @@
     imports = [
         inputs.nixos-hardware.nixosModules.common-cpu-amd
         inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-        inputs.nixos-hardware.nixosModules.common-cpu-amd-raphael-igpu
+        inputs.nixos-hardware.nixosModules.common-cpu-amd-raphael-igpu # LOANED PC FOR CHRISTMAS TRIP
         inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
         inputs.nixos-hardware.nixosModules.common-gpu-amd
         ../disko/mjolnir.nix
@@ -21,7 +21,10 @@
 
     boot = {
         extraModulePackages = with pkgs; [ btrfs-progs ];
+        # UM790
         initrd.availableKernelModules = [ "nvme" "xhci_pci" "ehci_pci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+        # 5700g
+        # initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
         initrd.kernelModules = [ "amdgpu" ];
         kernel.sysctl."vm.max_map_count" = 16777216;
         kernelModules = [ "kvm-amd" ];
