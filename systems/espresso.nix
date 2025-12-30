232172698183a6a30c21cf851921b35a3f236e0b
kaldyr <kaldyr@gmail.com>
2025-12-29 19:06:19 -0800

    Hardware swap take two

--- systems/espresso.nix
+++ systems/espresso.nix
@@ -3,7 +3,6 @@
     imports = [
         inputs.nixos-hardware.nixosModules.common-cpu-amd
         inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-        inputs.nixos-hardware.nixosModules.common-cpu-amd-raphael-igpu # TEMPORARY DURING CHRISTMAS
         inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
         inputs.nixos-hardware.nixosModules.common-gpu-amd
         ../disko/espresso.nix
@@ -17,10 +16,7 @@
 
     boot = {
         extraModulePackages = with pkgs; [ btrfs-progs ];
-        # UM790 - TEMPORARY DURING CHRISTMAS
-        initrd.availableKernelModules = [ "nvme" "xhci_pci" "ehci_pci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
-        # 5700g
-        # initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+        initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
         initrd.kernelModules = [ "amdgpu" ];
         kernel.sysctl."vm.max_map_count" = 16777216;
         kernelModules = [ "kvm-amd" ];
