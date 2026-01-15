8b63a56d5001d9dbe1c1710a88d6044a5655b04a
kaldyr <kaldyr@gmail.com>
2026-01-14 17:33:53 -0800

    Updates

--- programs/lazygit.nix
+++ programs/lazygit.nix
@@ -1,11 +1,11 @@
-{ lib, sysConfig, ... }: {
+{ lib, pkgs, sysConfig, ... }: {
 
     environment.persistence = lib.mkIf sysConfig.homeImpermanence {
         "/nix".users.${sysConfig.user}.directories = [ ".local/state/lazygit" ];
     };
 
     home-manager.users.${sysConfig.user} = { config, ... }: {
-        programs.lazygit.enable = true;
+        home.packages = with pkgs; [ lazygit ];
         xdg.configFile."lazygit/config.yml".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/lazygit/config.yml";
     };
 
