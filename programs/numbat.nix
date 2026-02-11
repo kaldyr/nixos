16e45b8e7f6f3a11f28559ddcc5355caf8bd643f
kaldyr <kaldyr@gmail.com>
2026-02-10 22:33:17 -0800

    Remove numbat

--- programs/numbat.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, sysConfig, ... }: {
-
-    home-manager.users.${sysConfig.user} = { config, ... }: {
-        home.packages = with pkgs; [ numbat ];
-        xdg.configFile."numbat/config.toml".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/numbat/config.toml";
-    };
-
-}
