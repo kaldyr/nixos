5a58339221d095c2266af37c8667e6b6814923df
kaldyr <kaldyr@gmail.com>
2026-02-11 18:30:32 -0800

    Cleanup

--- programs/wezterm.nix
+++
@@ -1,8 +0,0 @@
-{ sysConfig, ... }: {
-
-    home-manager.users.${sysConfig.user} = { config, ... }: {
-        programs.wezterm.enable = true;
-        xdg.configFile."wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/wezterm/wezterm.lua";
-    };
-
-}
