99e203dd7c528eadcab0d0707d34b44341d3a258
kaldyr <kaldyr@gmail.com>
2026-02-23 22:23:20 -0800

    Foot font config per machine

--- programs/foot.nix
+++ programs/foot.nix
@@ -3,7 +3,7 @@
     home-manager.users.${sysConfig.user} = { config, ... }: {
         home.packages = with pkgs; [ libsixel ];
         programs.foot.enable = true;
-        xdg.configFile."foot/foot.ini".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/foot/foot.ini";
+        xdg.configFile."foot/foot.ini".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/foot/${sysConfig.hostname}.ini";
         xdg.desktopEntries."footclient" = { name = "Foot Client"; noDisplay = true; };
         xdg.desktopEntries."foot-server" = { name = "Foot Server"; noDisplay = true; };
     };
