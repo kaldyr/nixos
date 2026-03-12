2d4f1ad7cb5951ad928206a4d6a04027614fc5ca
kaldyr <kaldyr@gmail.com>
2026-03-12 12:19:01 -0700

    Update Alias

--- programs/fish.nix
+++ programs/fish.nix
@@ -49,7 +49,7 @@
                 df = "duf";
                 du = "du -hs";
                 ln = "ln -i";
-                ls = "g -1 --color basic --dir-first --git --icon";
+                ls = "g -1 --color basic --dir-first --git --icon --sort .name";
                 ll = "ls --long";
                 la = "ls --long --almost-all";
                 lla = "ls --long --almost-all";
