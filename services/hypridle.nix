cdc55df6252c44281a000070933e0fbcdd410b6c
kaldyr <kaldyr@gmail.com>
2026-05-06 18:22:54 -0700

    Formatting

--- services/hypridle.nix
+++ services/hypridle.nix
@@ -3,14 +3,7 @@
     home-manager.users.${sysConfig.user} = { config, ... }: {
 
         home.packages = with pkgs; [ hypridle ];
-
-        # services.hypridle.enable = (
-        #     # Mjolnir hyprlock crash when left for minutes
-        #     if sysConfig.hostname == "mjolnir" then false
-        #     else true
-        # );
         services.hypridle.enable = true;
-
         xdg.configFile."hypr/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/hypr/hypridle.conf";
 
     };
