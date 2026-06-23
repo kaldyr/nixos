a19e2a09e6377125f4ffde0b297923c75300355b
kaldyr <kaldyr@gmail.com>
2026-06-22 20:38:16 -0700

    Hyprlock tweaks

--- programs/hyprlock.nix
+++ programs/hyprlock.nix
@@ -1,6 +1,5 @@
 { pkgs, sysConfig, ... }: {
 
-
     home-manager.users.${sysConfig.user} = { config, ... }: {
         home.packages = with pkgs; [ hyprlock ];
         xdg.configFile."hypr/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "/nix/config/dotfiles/hypr/hyprlock.conf";
