d305406e362da58466d0b9fba830f87c5e61172a
kaldyr <kaldyr@gmail.com>
2026-07-14 22:59:05 -0700

    Keyd revamp

--- services/keyd/default.nix
+++ services/keyd/default.nix
@@ -1,67 +1,83 @@
-{
+{ lib, pkgs, ... }:
+let
+    common = {
+        control = {
+            comma = "macro([b)";
+            "." = "macro(]b)";
+        };
+
+        layerCaps = {
+            # For WASD games, turn instead of strafe when layer active
+            a = "left";
+            d = "right";
+            w = "up";
+            s = "down";
+            # For fast vim arrow movements without exiting insert mode
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+            # Extra keybinds for games from the numpad
+            "1" = "kp1";
+            "2" = "kp2";
+            "3" = "kp3";
+            "4" = "kp4";
+            "5" = "kp5";
+            "z" = "kp6";
+            "q" = "kp7";
+            "e" = "kp8";
+            "r" = "kp9";
+            "t" = "kp0";
+            "f" = "kpminus";
+            "g" = "kpplus";
+            "v" = "kpdot";
+        };
+
+        main = {
+            capslock = "overload(layerCaps, esc)";
+            leftcontrol = "layer(control)";
+        };
+    };
+in {
+
+    environment.systemPackages = with pkgs; [ keyd ];
+
 	services.keyd = {
 		enable = true;
 
-		keyboards.default.settings = {
-			layerCaps = {
+		keyboards = {
+            default.settings = common;
 
-				# For WASD games, turn instead of strafe when layer active
-				a = "left";
-				d = "right";
-				w = "up";
-				s = "down";
+            air60 = {
+                ids = [ "19f5:3255:0d7157bb" ];
 
-				# Extra keybinds for games from the numpad
-				"1" = "kp1";
-				"2" = "kp2";
-				"3" = "kp3";
-				"4" = "kp4";
-				"5" = "kp5";
-				"z" = "kp6";
-				"q" = "kp7";
-				"e" = "kp8";
-				"r" = "kp9";
-				"t" = "kp0";
-				"f" = "kpminus";
-				"g" = "kpplus";
-				"v" = "kpdot";
+                settings = lib.recursiveUpdate common {
+                    main = {
+                        esc = "`";
+                        rightalt = "layer(layerRightAlt)";
+                        rightshift = "print";
+                    };
 
-				# For fast vim arrow movements without exiting insert mode
-				h = "left";
-				j = "down";
-				k = "up";
-				l = "right";
+                    layerRightAlt = {
+                        "1" = "brightnessdown";
+                        "2" = "brightnessup";
+                        "3" = "M-f";
+                        "4" = "M-r";
+                        "5" = "micmute";
+                        "6" = "M-p";
+                        "7" = "previoussong";
+                        "8" = "playpause";
+                        "9" = "nextsong";
+                        "0" = "mute";
+                        "minus" = "volumedown";
+                        "equal" = "volumeup";
+                    };
 
-				# Extra functionality for 60% keyboard
-				"delete" = "insert";
-			};
-
-			layerRightAlt = {
-				# Media Keys (Mac order to match Nuphy Air60)
-				"1" = "brightnessdown";
-				"2" = "brightnessup";
-				"3" = "M-f";
-				"4" = "M-r";
-				"5" = "micmute";
-				"6" = "M-p";
-				"7" = "previoussong";
-				"8" = "playpause";
-				"9" = "nextsong";
-				"0" = "mute";
-				"minus" = "volumedown";
-				"equal" = "volumeup";
-			};
-
-			main = {
-				capslock = "overload(layerCaps, esc)";
-				rightalt = "overload(layerRightAlt, rightalt)";
-				leftcontrol = "layer(control)";
-			};
-
-			control = {
-				comma = "macro([b)";
-				"." = "macro(]b)";
-			};
-		};
+                    layerCaps = {
+                        "delete" = "insert";
+                    };
+                };
+            };
+        };
 	};
 }
