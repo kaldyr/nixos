46b12e8cf2eb9604bb272f9c3185261aef3f5644
kaldyr <kaldyr@gmail.com>
2026-08-24 18:55:11 -0700

    Keepmenu finally up to date in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 pkgs: {
-  keepmenu = pkgs.callPackage ./keepmenu.nix { };
   maple-mono-custom = pkgs.callPackage ./maple-mono.nix { };
   openstarbound = pkgs.callPackage ./openstarbound { };
   wayland-push-to-talk-fix = pkgs.callPackage ./wayland-push-to-talk-fix.nix { };
