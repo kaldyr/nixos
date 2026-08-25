f928dae925ff813199235d371e772a0947e4ae98
kaldyr <kaldyr@gmail.com>
2026-08-24 18:55:24 -0700

    Bluetooth tweaks

--- services/pipewire/default.nix
+++ services/pipewire/default.nix
@@ -11,22 +11,33 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."99-bt-latency" = {
+      "context.properties" = {
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
+
     wireplumber = {
       enable = true;
-      extraConfig = {
-        "10-bluez" = {
-          "monitor.bluez.properties" = {
-            "bluez5.enable-sbc-xq" = true;
-            "bluez5.enable-msbc" = true;
-            "bluez5.enable-hw-volume" = true;
-            "bluez5.codecs" = [
-              "sbc"
-              "sbc_xq"
-              "aac"
-              "aptx"
-              "aptx_hd"
-            ];
-          };
+
+      extraConfig."10-bluez" = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+
+          "bluez5.codecs" = [
+            "aac"
+            "sbc"
+            "sbc_xq"
+          ];
+        };
+
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
         };
       };
     };
