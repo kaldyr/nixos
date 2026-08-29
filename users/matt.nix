66e0f9464a281be5d0533e21e208ee5037efb6bb
kaldyr <kaldyr@gmail.com>
2026-08-29 13:57:55 -0700

    Update keys

--- users/matt.nix
+++ users/matt.nix
@@ -3,6 +3,7 @@
     home.sessionVariables = {
       EDITOR = "nvim";
       VISUAL = "nvim";
+      SOPS_AGE_KEY_FILE = "/state/age/keys.txt";
     };
 
     xdg.mimeApps.defaultApplications = lib.mkForce {
