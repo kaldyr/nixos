5bb7a88b6e89cddf8a9e57642155d9f3ae0fd396
kaldyr <kaldyr@gmail.com>
2026-09-04 13:30:27 -0700

    Per-machine easyeffects autoload configuration

--- programs/easyeffects/default.nix
+++ programs/easyeffects/default.nix
@@ -6,7 +6,15 @@
   home-manager.users.${sysConfig.user} = { config, ... }: {
     services.easyeffects.enable = true;
 
-    home.file.".local/share/easyeffects".source =
-      config.lib.file.mkOutOfStoreSymlink "/nix/config/programs/easyeffects/config";
+    home.file = {
+      ".local/share/easyeffects/input".source =
+        config.lib.file.mkOutOfStoreSymlink "/nix/config/programs/easyeffects/config/input";
+
+      ".local/share/easyeffects/output".source =
+        config.lib.file.mkOutOfStoreSymlink "/nix/config/programs/easyeffects/config/output";
+
+      ".local/share/easyeffects/autoload".source =
+        config.lib.file.mkOutOfStoreSymlink "/nix/config/programs/easyeffects/config/${sysConfig.hostname}";
+    };
   };
 }
